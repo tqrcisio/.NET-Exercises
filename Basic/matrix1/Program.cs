db7c0b903c6723ce79d7a8f678292c44b31d2b46
Tarcisio Ferreira <tarcisiosaf@gmail.com>
2023-12-27 16:14:06 -0300

    matrix1 concluded

---
+++ Basic/matrix1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int matrixIndex = 0; matrixIndex < n; matrixIndex++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int valueIndex = 0; valueIndex < values.Length; valueIndex++)
+                {
+                    int value = int.Parse(values[valueIndex]);
+                    matrix[matrixIndex, valueIndex] = value;
+                }
+            }
+
+            System.Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.Write(matrix[i, i] + " ");
+            }
+
+
+            int countNegative = 0;
+            for (int x = 0; x < n; x++)
+                for (int y = 0; y < n; y++)
+                {
+                    if (matrix[x, y] < 0)
+                    {
+                        countNegative++;
+                    }
+                }
+            System.Console.WriteLine("Negative numbers = " + countNegative);
+        }
+    }
+}
---
+++ Basic/matrix1/README.md
@@ -0,0 +1,13 @@
+# Desafio list1 C#
+
+- Fazer um programa para ler um número inteiro N e uma matriz de
+  ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+  principal e a quantidade de valores negativos da matriz.
+
+# Exemplo de como o programa deverá funcionar
+
+| Input:  |              Output: |
+| :------ | -------------------: |
+| 3       |       Main diagonal: |
+| 5 -3 10 |               5 8 -4 |
+| 7 9 -4  | Negative numbers = 2 |
